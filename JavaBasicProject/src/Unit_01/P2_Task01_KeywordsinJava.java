d87059f2275dd4683d85c9b263644f0fa90aa789
ARNAV GUPTA <ARNAV GUPTA@DESKTOP-2LLBRBU>
2022-03-24 20:33:53 +0530

    Third Commit!

---
+++ JavaBasicProject/src/Unit_01/P2_Task01_KeywordsinJava.java
@@ -0,0 +1,20 @@
+package Unit_01;
+
+public class P2_Task01_KeywordsinJava {
+	class student{
+		int b;
+	}
+	public static void main(String[] args)
+	{
+		//Syntax error on token "float", invalid VariableDeclaratorId
+		int float=2.5f;
+		
+		//student cannot be resolved to a variable
+		student int= new student;
+	}
+	//Syntax error on token "char", Identifier expected after this token
+	char();
+
+}
+//Syntax error on tokens, delete these tokens
+class int();
